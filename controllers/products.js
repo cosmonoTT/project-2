c5a07edf672c76d7613d7d42d55adeee7658ff3d
cosmonot <oa.ae1010@gmail.com>
2024-06-15 12:59:10 -0500

    index route res.sends hello world

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const router = express.Router
-const Product = require('../models/products.js')
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/products.js");
 
 // I.N.D.U.C.E.S
 
-module.exports = router
+router.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+module.exports = router;
