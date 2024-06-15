94331c4dfa2a63ca097847291fcdb8faee0603af
cosmonot <oa.ae1010@gmail.com>
2024-06-15 13:13:37 -0500

    New route res.sends a string

--- controllers/products.js
+++ controllers/products.js
@@ -4,8 +4,24 @@ const Product = require("../models/products.js");
 
 // I.N.D.U.C.E.S
 
+// INDEX
 router.get('/', (req, res) => {
     res.send("hello world")
 })
+
+// NEW
+router.get('/new', (req, res) => {
+    res.send("New Route Works")
+})
+
+// DELETE
+
+// UPDATE
+
+// CREATE
+
+// EDIT
+
+// SHOW
 
 module.exports = router;
