245861bc712bc5689f6a6932f8968b8219500d4a
cosmonot <oa.ae1010@gmail.com>
2024-06-15 12:21:58 -0500

    Server is running on port

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+const express = require("express");
+const app = express();
+const port = 4000;
+
+// I.N.D.U.C.E.S
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
