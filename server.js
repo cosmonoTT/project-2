e0ac3618fce24f88a491c2a12a44237240eca525
cosmonot <oa.ae1010@gmail.com>
2024-06-15 12:30:35 -0500

    connect to mongoose

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 // Dependencies
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 4000;
+
+const mongoURI = "mongodb://127.0.0.1:27017/products";
+
+async function connectToMongo() {
+  try {
+    mongoose.connect(mongoURI);
+    console.log("connected to mongoose");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+connectToMongo();
 
 // I.N.D.U.C.E.S
 
