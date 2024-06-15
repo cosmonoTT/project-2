1ccbb91b8ba657900d00e15d92fa9014dd6e62ca
cosmonot <oa.ae1010@gmail.com>
2024-06-15 15:14:25 -0500

    show route renders show.ejs with product information populated

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const router = express.Router();
 const Product = require("../models/products.js");
-const seedData = require("../models/seedData.js")
+const seedData = require("../models/seedData.js");
 
 // I.N.D.U.C.E.S
 
 // INDEX
 router.get("/", async (req, res) => {
-    const allProducts = await Product.find({})
-    console.log(allProducts)
-  res.render("index.ejs", {
-    products: allProducts
-  })
+  try {
+    const allProducts = await Product.find({});
+    console.log(allProducts);
+    res.render("index.ejs", {
+      products: allProducts,
+    });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    mongoose.connection.close();
+  }
 });
 
 // NEW
@@ -26,8 +32,8 @@ router.get("/new", (req, res) => {
 
 // CREATE
 router.post("/", async (req, res) => {
-    // res.send("received")
-//   parsing data into booleans
+  // res.send("received")
+  //   parsing data into booleans
   if (req.body.finishedProduct === "on") {
     req.body.finishedProduct = true;
   } else {
@@ -41,22 +47,32 @@ router.post("/", async (req, res) => {
   }
 
   try {
-    const createdProduct = await Product.create(req.body)
-    console.log(createdProduct)
-    res.redirect('/products')
+    const createdProduct = await Product.create(req.body);
+    console.log(createdProduct);
+    res.redirect("/products");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   } finally {
-    mongoose.connection.close
+    mongoose.connection.close;
   }
 });
 
 // EDIT
 
 // SHOW
-router.get('/:id', (req, res) => {
-    res.send("Product Show Page")
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const productId = await Product.findById(req.params.id);
+    console.log(productId)
+    res.render("show.ejs", {
+      product: productId,
+    });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    mongoose.connection.close();
+  }
+});
 
 // async function seedMyData(seedData) {
 //     try {
